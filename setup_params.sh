ad337b3abafc8b47518742de32170bb7deb5a17c
pannagk <125200467+pannagk@users.noreply.github.com>
2023-05-23 15:03:25 +0530

    Update setup_params.sh

--- setup_params.sh
+++ setup_params.sh
@@ -5,6 +5,8 @@ user=$(whoami)
 #Enter a prefix to identify the resources in aws and on cdp. By default it is set to cdp-env-<your username>
 prefix=cdp-env-${user} 
 
+# AWS Profile Details
+aws_profile_name=cdp-sandbox-env
 aws_region=ap-south-1
 
 cdp_runtime="7.2.16"
